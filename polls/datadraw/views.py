90f864432931d351b3ca8335f3fb7a6fec2e0bfa
johnyssho <hon.brod@email.cz>
2023-11-24 18:59:21 +0100

    visualized smthing, will try and make sense of it

--- polls/datadraw/views.py
+++ polls/datadraw/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
+import matplotlib.pyplot as plt
+import numpy as np
+from io import StringIO
+
 
 # Create your views here.
 
 def TestView(request):
-  return render(request, 'index.html')
+
+  x = np.arange(0,np.pi*3,.1)
+  y = np.sin(x)
+
+  fig = plt.figure()
+  plt.plot(x,y)
+
+  imgdata = StringIO()
+  fig.savefig(imgdata, format='svg')
+  imgdata.seek(0)
+
+  data = imgdata.getvalue()
+
+  context = { 'fig' : data } 
+  return render(request, 'index.html', context)
